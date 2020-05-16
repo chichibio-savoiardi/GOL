51f8918dbfb5051c6bd0fdb80e2e2b1cf878642e
sphere4779 <iuricube@gmail.com>
2020-05-16 17:12:20 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+import com.company.javaUtils.*;
+
+public class Main {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //todo
+    }
+}
+/* fin */
---
+++ src/com/company/javaUtils/basicUtils.java
@@ -0,0 +1,12 @@
+package com.company.javaUtils;
+
+import java.util.*;
+import java.lang.*;
+
+public class basicUtils {
+    static Scanner in = new Scanner(System.in);
+
+    public static void funx() {
+        //code
+    }
+}
